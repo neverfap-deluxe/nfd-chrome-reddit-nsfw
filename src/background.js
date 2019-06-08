fde9c40e9cd980358ebfc7f4795cdb17b8f6bb6b
dottjt <julius.reade@gmail.com>
2019-06-08 11:28:49 +1000

    yus

--- src/background.js
+++ src/background.js
@@ -5,7 +5,7 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function () {
-  fetchAudioMeditationsList();
+  // fetchAudioMeditationsList();
 
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
@@ -18,34 +18,34 @@ chrome.runtime.onInstalled.addListener(function () {
   });
 });
 
-chrome.runtime.onStartup.addListener(function () {
-  fetchAudioMeditationsList();
-});
+// chrome.runtime.onStartup.addListener(function () {
+//   fetchAudioMeditationsList();
+// });
 
-chrome.runtime.onUpdateAvailable.addListener(function () {
-  fetchAudioMeditationsList();
-});
+// chrome.runtime.onUpdateAvailable.addListener(function () {
+//   fetchAudioMeditationsList();
+// });
 
-function fetchAudioMeditationsList() {
-  const meditationsList = [
-    {
-      title: "Observe Your Senses (pilot)",
-      mp3Url: "https://s3.castbox.fm/1d/10/e3/9239a649bb85addc36d0754c3a.mp3",
-      position: 0,
-    },
-    {
-      title: "Observe Your Senses",
-      mp3Url: "https://s3.castbox.fm/61/6f/59/95055247d0b6fe18ccc053aaec.mp3",
-      position: 1,
-    }
-    // {
-    //   title: "The Cloud Of Sensation",
-    //   mp3Url: "https://s3.castbox.fm/1d/10/e3/9239a649bb85addc36d0754c3a.mp3",
-    //   position: 2,
-    // },
-  ];
+// function fetchAudioMeditationsList() {
+//   const meditationsList = [
+//     {
+//       title: "Observe Your Senses (pilot)",
+//       mp3Url: "https://s3.castbox.fm/1d/10/e3/9239a649bb85addc36d0754c3a.mp3",
+//       position: 0,
+//     },
+//     {
+//       title: "Observe Your Senses",
+//       mp3Url: "https://s3.castbox.fm/61/6f/59/95055247d0b6fe18ccc053aaec.mp3",
+//       position: 1,
+//     }
+//     // {
+//     //   title: "The Cloud Of Sensation",
+//     //   mp3Url: "https://s3.castbox.fm/1d/10/e3/9239a649bb85addc36d0754c3a.mp3",
+//     //   position: 2,
+//     // },
+//   ];
 
-  chrome.storage.sync.set({ meditationsList, selectedMeditation: meditationsList[0] }, function (data) {
-    console.log(data);
-  });
-}
+//   chrome.storage.sync.set({ meditationsList, selectedMeditation: meditationsList[0] }, function (data) {
+//     console.log(data);
+//   });
+// }
--- src/popup.js
+++ src/popup.js
@@ -1,11 +1,11 @@
 let wipePage = document.getElementById('wipePage');
 let close__popup = document.getElementById('close__popup');
 
-let previous__meditation__button = document.getElementById('previous__meditation__button');
-let next__meditation__button = document.getElementById('next__meditation__button');
+// let previous__meditation__button = document.getElementById('previous__meditation__button');
+// let next__meditation__button = document.getElementById('next__meditation__button');
 
-let meditation__audio__player = document.getElementById('meditation__audio__player');;
-let meditation__title__single = document.getElementById('meditation__title__single');;
+// let meditation__audio__player = document.getElementById('meditation__audio__player');;
+// let meditation__title__single = document.getElementById('meditation__title__single');;
 
 
 
@@ -14,70 +14,70 @@ close__popup.onclick = function(element) {
   window.close();
 };
 
-chrome.storage.sync.get('selectedMeditation', function (data) {
-  setNewMeditation(data.selectedMeditation);
+// chrome.storage.sync.get('selectedMeditation', function (data) {
+//   setNewMeditation(data.selectedMeditation);
 
-  previous__meditation__button.style.visibility = 'hidden';
-});
+//   previous__meditation__button.style.visibility = 'hidden';
+// });
 
-previous__meditation__button.onclick = function(element) {
-  chrome.storage.sync.get(['selectedMeditation', 'meditationsList'], function (data) {
-    console.log('here');
-    const { isFirstOrLast, newMeditation } = getNextMeditation(data.selectedMeditation, data.meditationsList, -1);
+// previous__meditation__button.onclick = function(element) {
+//   chrome.storage.sync.get(['selectedMeditation', 'meditationsList'], function (data) {
+//     console.log('here');
+//     const { isFirstOrLast, newMeditation } = getNextMeditation(data.selectedMeditation, data.meditationsList, -1);
 
-    console.log(isFirstOrLast, newMeditation);
-    disappearOrAppearFirstAndLast(isFirstOrLast, next__meditation__button);
-    setNewMeditation(newMeditation);
-  });
-};
+//     console.log(isFirstOrLast, newMeditation);
+//     disappearOrAppearFirstAndLast(isFirstOrLast, next__meditation__button);
+//     setNewMeditation(newMeditation);
+//   });
+// };
 
-next__meditation__button.onclick = function (element) {
-  chrome.storage.sync.get(['selectedMeditation', 'meditationsList'], function (data) {
-    const { isFirstOrLast, newMeditation } = getNextMeditation(data.selectedMeditation, data.meditationsList, 1);
+// next__meditation__button.onclick = function (element) {
+//   chrome.storage.sync.get(['selectedMeditation', 'meditationsList'], function (data) {
+//     const { isFirstOrLast, newMeditation } = getNextMeditation(data.selectedMeditation, data.meditationsList, 1);
 
-    disappearOrAppearFirstAndLast(isFirstOrLast, previous__meditation__button);
-    setNewMeditation(newMeditation);
-  });
-};
+//     disappearOrAppearFirstAndLast(isFirstOrLast, previous__meditation__button);
+//     setNewMeditation(newMeditation);
+//   });
+// };
 
-wipePage.onclick = function(element) {
-  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    chrome.tabs.executeScript(tabs[0].id, {
-      code: 'document.body.style.display = "none";',
-    });
-  });
-};
+// wipePage.onclick = function(element) {
+//   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+//     chrome.tabs.executeScript(tabs[0].id, {
+//       code: 'document.body.style.display = "none";',
+//     });
+//   });
+// };
 
-function getNextMeditation(selectedMeditation, meditationList, movement) {
-  const isFirstOrLast = selectedMeditation.position === 0 || meditationList.length === (selectedMeditation.position + 1) ? true : false;
-  // the problem is that this is always true.
+// function getNextMeditation(selectedMeditation, meditationList, movement) {
+//   const isFirstOrLast = selectedMeditation.position === 0 || meditationList.length === (selectedMeditation.position + 1) ? true : false;
+//   // the problem is that this is always true.
 
-  const newPosition = selectedMeditation.position + movement;
-  const validateNewPosition = newPosition < 0 || newPosition >= meditationList.length ? selectedMeditation.position : newPosition;
-  const newMeditation = meditationList[validateNewPosition];
+//   const newPosition = selectedMeditation.position + movement;
+//   const validateNewPosition = newPosition < 0 || newPosition >= meditationList.length ? selectedMeditation.position : newPosition;
+//   const newMeditation = meditationList[validateNewPosition];
 
-  chrome.storage.sync.set({ selectedMeditation: newMeditation }, function (data) {});
+//   chrome.storage.sync.set({ selectedMeditation: newMeditation }, function (data) {});
 
-  return {
-    isFirstOrLast,
-    newMeditation,
-  }
-}
+//   return {
+//     isFirstOrLast,
+//     newMeditation,
+//   }
+// }
 
-function setNewMeditation(selectedMeditation) {
-  const selectedMeditationUrl = selectedMeditation.mp3Url;
-  const selectedMeditationTitle = selectedMeditation.title;
+// function setNewMeditation(selectedMeditation) {
+//   const selectedMeditationUrl = selectedMeditation.mp3Url;
+//   const selectedMeditationTitle = selectedMeditation.title;
 
-  meditation__audio__player.src = selectedMeditationUrl;
-  meditation__title__single.innerHTML = selectedMeditationTitle;
+//   meditation__audio__player.src = selectedMeditationUrl;
+//   meditation__title__single.innerHTML = selectedMeditationTitle;
 
-  meditation__audio__player.load();
-}
+//   meditation__audio__player.load();
+// }
 
-function disappearOrAppearFirstAndLast(isFirstOrLast, component) {
-  if (isFirstOrLast) {
-    component.style.visibility = 'hidden';
-  } else {
-    component.style.visibility = 'visible';
-  }
-}
+// function disappearOrAppearFirstAndLast(isFirstOrLast, component) {
+//   if (isFirstOrLast) {
+//     component.style.visibility = 'hidden';
+//   } else {
+//     component.style.visibility = 'visible';
+//   }
+// }
